59ada54cd99e9ded7a5aa8996f754b9dd87b845c
renjith <renjithforever@gmail.com>
2014-03-28 22:45:02 +0530

    zcat:...

--- zcat.py
+++ zcat.py
@@ -2,11 +2,11 @@
 ZCAT, Send multiple files over your network!
 =============================================
 renjith 2014, renjithforever@gmail.com
-license: use as u like on your own risk!
+license: use as u like at your own risk!
 
 ___+[ABOUT]+_______
 nc or NETCAT is a cool linux tool that can send files over your network.
-Howerver it doesnt allow u to send multiple files, in which case you have
+However it doesnt allow u to send multiple files, in which case you have
 to tar/compress the files.
 
 ZCAT bridges this gap by allowing you to send multiple files in one go!
@@ -15,15 +15,17 @@ ZCAT uses nc!
 ___+[NOTE]+_______
 As of now doesnt handle nested directories (not recursive)
 
-__+[REQUIREMNTS]+_____
+__+[REQUIREMENTS]+_____
 python, nc, therefore linux!
 
 __+[USAGE]+____
-sender must start zcat first, inform the receiver ur IP
+sender must start zcat first, inform the receiver his/her IP
 and start zcat at the receivers end!
 
-interactive mode (prefered):
+Interactive mode (preferred):
 	python zcat.py
+	-
+	all prompts accept defaults by pressing [ENTER].
 
 send files:
 	python zcat.py -s <path-to-folder>
@@ -31,10 +33,21 @@ send files:
 receive files:	
 	python zcat.py -r <host-ip>
 
+__+[TODO]+___
+* add transfer rate/progress to both sender and receiver.
+* allow cloning entire nested directory structures.
+* provide fine tuned operation through command-line arguments.
+
 ___[]____
 [[ascii art: http://patorjk.com/software/taag/]]
 
+__[TRIVIA]_____
+ZCAT was written on a warm Sunday(march 2014) evening 
+when i was sick with a real bad ass diarrhea attack!
+However, the script now is more than a one-night-hack.
 """
+
+
 import subprocess
 import os
 import re
@@ -56,11 +69,16 @@ ERASE_LINE = '\x1b[2K'
 #----------------------------------------------
 
 def zcat_send_record(record_fileName,portNum):
+	"""
+	"""
+
 	command="nc -l "+str(portNum)+" <"+record_fileName
 	process=subprocess.Popen(command,shell=True, stdout=subprocess.PIPE)
 	process.wait()
 
 def zcat_send_files(record,initPortNum):
+	"""
+	"""
 		
 	for portNum,fileName in record.items():
 		
@@ -68,7 +86,8 @@ def zcat_send_files(record,initPortNum):
 		process=subprocess.Popen(command,shell=True, stdout=subprocess.PIPE)
 
 def zcat_get_record(record_fileName,host,portNum):
-		
+	"""
+	"""
 	
 	dummy=open(record_fileName,"w")
 	dummy.write('')
@@ -96,6 +115,8 @@ def zcat_get_record(record_fileName,host,portNum):
 	return message,record
 
 def zcat_get_files(record,host):
+	"""
+	"""
 
 	for portNum,fileName in record.items():
 		fileName=fileName.split("/")[-1]
@@ -163,7 +184,7 @@ def get_params():
 	action=''
 	path=''
 	host=''
-	message="file list..."
+	message=""
 	port=1200
 
 
@@ -238,6 +259,9 @@ def get_params():
 
 	
 def show_record(record):
+	"""
+	"""
+
 	for i,fileName in list(enumerate(record.values())):
 		print (i+1),'"'+fileName.split("/")[-1], '\t',
 
@@ -283,5 +307,6 @@ if __name__ == "__main__":
 			zcat_get_files(record,host)
 			print  "[:)]TRANSFER COMPLETE!"
 		else:
-			print "[!!] tell your friend to run `pkill nc` to close all ports he opened for you!"
+			print "[!!] tell your friend to run `pkill nc` \
+					to close all ports he opened for you!"
 
